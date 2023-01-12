daf917b4b36c5e3838ef48784928db116c529d2e
buiminh1995 <buiminh2000@gmail.com>
2023-01-12 15:41:23 -0500

    add context provider + navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
+import {Toaster} from 'react-hot-toast'
 import Layout from '../components/Layout'
+import { StateContext } from '../context/StateContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
