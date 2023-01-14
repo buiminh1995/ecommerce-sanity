7e3cd867a0afd6a55459c0ee8ba5a2cbacbb3be9
buiminh1995 <buiminh2000@gmail.com>
2023-01-13 19:17:31 -0500

    toggleCartItemQuanitity func

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
+    let foundProduct;
+    let index;
+
     const onAdd = (product, quantity) => {
         //check if product already in cart
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
@@ -21,23 +24,42 @@ export const StateContext = ({ children }) => {
         
         //if product already in cart
         if(checkProductInCart) {
-          const updatedCartItems = cartItems.map((cartProduct) => {
+            const updatedCartItems = cartItems.map((cartProduct) => {
             if(cartProduct._id === product._id) return {
-              ...cartProduct,
-              quantity: cartProduct.quantity + quantity
+                ...cartProduct,
+                quantity: cartProduct.quantity + quantity
             }
-          })
-    
-          setCartItems(updatedCartItems);
+        })
+            setCartItems(updatedCartItems);
         } else {
-          product.quantity = quantity;
-          //add new item to cart
-          setCartItems([...cartItems, { ...product }]);
+            //set new key to object product: quantity
+            product.quantity = quantity;
+            //add new item to cart
+            setCartItems([...cartItems, { ...product }]);
         }
         
         //notification appears on page when successfully added product to cart
         toast.success(`${qty} ${product.name} added to the cart.`);
-      }
+    }
+    
+    const toggleCartItemQuanitity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id)
+        //index = cartItems.findIndex((product) => product._id === id);
+        //never mutate the state with equal = sign, so don't use index to access cartItems
+        const newCartItems = cartItems.filter((item) => item._id !== id)
+    
+        if(value === 'inc') {
+          setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
+          setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+          setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+        } else if(value === 'dec') {
+          if (foundProduct.quantity > 1) {
+            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+          }
+        }
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1);
